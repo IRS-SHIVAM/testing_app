cb002b42b20ef3df5279098be3d269ab5ed1a4ee
IRS-SHIVAM <shivam.mistry@ishitva.in>
2025-02-21 16:24:04 +0530

    cpu gpu temperature added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,23 @@ import { Component, ElementRef ,ViewChild } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  @ViewChild('imgContainer') imgContainer!:ElementRef;
 
   title = 'portal';
+  ejectedCount:number = 45126;
+  rejectedCount:number = 124587;
+  gpuTemperature:number = 65 ;
+  cpuTemperature:number = 62 ;
+
   constructor(){
-    // setInterval(()=>{
-    //   console.log();
-    // },4000);
+    setInterval(()=>{
+      this.updateEjectedValues();
+      this.updateRejectedValues();
+    },400);
+  }
+  updateEjectedValues(){
+    this.ejectedCount += Math.floor(Math.random() * 10); 
+  }
+  updateRejectedValues(){
+    this.rejectedCount += Math.floor(Math.random() * 10); 
   }
 }
