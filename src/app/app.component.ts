804e17ba870ddee16cd98099109f4c840f703660
IRSVareen <vareen.patel@ishitva.in>
2025-02-21 15:12:44 +0530

    cpu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,18 @@ import { Component, ElementRef ,ViewChild } from '@angular/core';
 })
 export class AppComponent {
   @ViewChild('imgContainer') imgContainer!:ElementRef;
-
+  ejectedCount = 3434 
+  rejectedCount = 456475
+  cpuTemp = 23.3
   title = 'portal';
   constructor(){
-    // setInterval(()=>{
-    //   console.log();
-    // },4000);
+    setInterval(()=>{
+      // this.updateValues()
+    },400);
   }
+
+  // updateValues(){
+  //   this.ejectedCount += Math.floor(Math.random()*10)
+  //   this.rejectedCount += Math.floor(Math.random() * 20)
+  // }
 }
