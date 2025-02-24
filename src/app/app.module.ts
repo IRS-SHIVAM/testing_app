d1d46473a8dee330b6d5e015984477c552932662
IRS-SHIVAM <shivam.mistry@ishitva.in>
2025-02-24 10:14:14 +0530

    bug fixed(margin)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NewPageComponent } from './new-page/new-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NewPageComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
