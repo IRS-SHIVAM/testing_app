2f03a05c14c5669f55181634fd70ce08b1a0b81d
IRS-SHIVAM <shivam.mistry@ishitva.in>
2025-02-25 15:26:22 +0530

    new component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MachineInfoComponent } from './components/machine-info/machine-info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MachineInfoComponent
   ],
   imports: [
     BrowserModule,
